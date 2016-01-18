4dd24faf7f12eac1392d7aa822af0934eed66223
James <james@james-HP-G62-Notebook-PC.(none)>
2016-01-18 11:16:12 -0700

    Code Commenting, Liscence Info

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import SocketServer
 import os.path
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Modified 2016 James Cadek
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,10 +29,9 @@ import os.path
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-# referenced the following document on file parsing:
-# http://www.dotnetperls.com/file-python
 class MyWebServer(SocketServer.BaseRequestHandler):
 
+    
     def parseRequest(self, request):
 	request = request.split("\r\n")
 	
@@ -43,17 +43,22 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 	mimetype = ""
 	outString = ""
 	
+	#Filter lines and search for specific headers
 	for i in range(0,len(request)):
+		#Pull Get request
 		if (request[i][:4] == "GET "):
 			attributes = request[i].split(" ")
 			verb = attributes[0]
 			directory = attributes[1]
 			protocol = attributes[2]
-		##elif (request[i][:12].lower() == "content-type"):
-		##	attributes = request[i].replace(" ","")
-		##	attributes = attributes.split(":")
-		##	mimetype = attributes[1]
+		
+		# Unused code that would parse out Content-Type Header
+		#elif (request[i][:12].lower() == "content-type"):
+		#	attributes = request[i].replace(" ","")
+		#	attributes = attributes.split(":")
+		#	mimetype = attributes[1]
 	
+	#Determines mimetype, and if a directory ends without a filetype, it adds index.html
 	if(directory[-1] == "/"):
 		directory = directory + "index.html"
 		mimetype = "text/html"
@@ -62,22 +67,27 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 	elif(directory[-4:] == ".css"):
 		mimetype = "text/css"
 	else:
-		directory = directory + "/"
+		directory = directory + "/index.html"
 	
+	#Add root and turn string into list
 	directory = "www" + directory 	
 	directory = directory.split("/")
 	fileExist = True
 	finalDepth = 0
 	path = ""
+	#Navigate through file path step by step and make sure every level exists
 	for level in range(1,len(directory)):
 		path = ""
 		depth = 0;
 		for deep in range(0,level):
+			#Skip empty levels
 			if(directory[deep] != ""):
 				depth += 1
+				#If current directory token is used, collapse it
 				if (directory[deep] == "."):
 					directory[deep] = ""
 					depth -= 1
+				#If parent directory token is used, remove the step that navigates you into the current level.
 				if (directory[deep] == ".."):
 					directory[deep] = ""
 					directory[deep - 1] = ""
@@ -88,6 +98,7 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 			finalDepth = depth
 		
 		exists = os.path.exists(path);
+		#If a directory does not exist, or the path levels the assignment www space, 404
 		if (exists == False):
 			fileExist = False
 		if (depth <= 0):
@@ -97,6 +108,7 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 	if (fileExist == False):
 		return False
 	elif (fileExist == True):
+		#Build final state of the filepath after removing tokens
 		path = ""
 		for deep in range(0,len(directory)):
 			if(directory[deep] != ""):
@@ -112,7 +124,7 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 				if (deep != len(directory) -1):
 					path = path + "/"
 			finalDepth = depth
-		
+		#Open file, add content-type header, append html body from file into output
 		newfile = open(path, "r")
 		outString = "Content-Type:" + mimetype + "\r\n\r\n"
 		for line in newfile.readlines():
